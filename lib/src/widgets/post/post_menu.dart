96deecceb7ce407c80de8caa20145c3b4ca91aaa
Tyagi Anurag <anurag.tyagi@likeminds.community>
2023-07-26 19:15:56 +0530

    Removed report integration from menu

--- lib/src/widgets/post/post_menu.dart
+++ lib/src/widgets/post/post_menu.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:likeminds_feed/likeminds_feed.dart';
 import 'package:likeminds_feed_ui_fl/src/utils/theme.dart';
-import 'package:likeminds_feed_ui_fl/src/widgets/common/buttons/text_button.dart';
 import 'package:likeminds_feed_ui_fl/src/widgets/common/icon/icon.dart';
 import 'package:likeminds_feed_ui_fl/src/widgets/common/text/text_view.dart';
 
@@ -21,46 +20,61 @@ class LMPostMenu extends StatelessWidget {
   final bool isFeed;
   final Function(int)? onSelected;
 
+  void removeReportIntegration() {
+    menuItems.removeWhere((element) {
+      return element.id == 4 || element.id == 7;
+    });
+    print(menuItems);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Builder(builder: (context) {
-        return PopupMenuButton<int>(
-          onSelected: onSelected,
-          itemBuilder: (context) => menuItems
-              .map(
-                (element) => PopupMenuItem(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 12,
-                    vertical: 10,
-                  ),
-                  height: 48,
-                  value: element.id,
-                  child: children?[element.id] ??
-                      LMTextView(
-                        text: element.title,
-                        textStyle: const TextStyle(
-                          color: kGreyColor,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                        ),
+    removeReportIntegration();
+    return Builder(
+      builder: (context) {
+        if (menuItems.isEmpty) {
+          return Container();
+        }
+        return SizedBox(
+          child: Builder(builder: (context) {
+            return PopupMenuButton<int>(
+              onSelected: onSelected,
+              itemBuilder: (context) => menuItems
+                  .map(
+                    (element) => PopupMenuItem(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 10,
                       ),
-                ),
-              )
-              .toList(),
-          color: kWhiteColor,
-          child: menuIcon ??
-              const SizedBox(
-                height: 24,
-                width: 24,
-                child: Icon(
-                  Icons.more_horiz,
-                  color: kGrey1Color,
-                  size: 24,
-                ),
-              ),
+                      height: 48,
+                      value: element.id,
+                      child: children?[element.id] ??
+                          LMTextView(
+                            text: element.title,
+                            textStyle: const TextStyle(
+                              color: kGreyColor,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                    ),
+                  )
+                  .toList(),
+              color: kWhiteColor,
+              child: menuIcon ??
+                  const SizedBox(
+                    height: 24,
+                    width: 24,
+                    child: Icon(
+                      Icons.more_horiz,
+                      color: kGrey1Color,
+                      size: 24,
+                    ),
+                  ),
+            );
+          }),
         );
-      }),
+      },
     );
   }
 }
