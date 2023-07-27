46960361dbdc55625dd30708d11689be6e4467b8
Divyansh Gandhi <120566424+divyanshgandhilm@users.noreply.github.com>
2023-07-27 14:39:24 +0530

    Updating feed SDK

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,14 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
-<<<<<<< Updated upstream
-
-void RegisterPlugins(flutter::PluginRegistry* registry) {
-=======
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
->>>>>>> Stashed changes
 }
