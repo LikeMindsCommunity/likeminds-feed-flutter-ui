cce139c1acd02abe22ec1ef7b91cac9fd03bc3ae
Anurag Tyagi <anurag.tyagi@likeminds.community>
2023-08-10 00:10:53 +0530

    Added placement option in text button

--- lib/src/widgets/common/buttons/text_button.dart
+++ lib/src/widgets/common/buttons/text_button.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:likeminds_feed_ui_fl/likeminds_feed_ui_fl.dart';
 import 'package:likeminds_feed_ui_fl/src/widgets/common/icon/icon.dart';
 
+enum LMIconPlacement { start, end }
+
 class LMTextButton extends StatefulWidget {
   const LMTextButton({
     super.key,
@@ -16,6 +18,7 @@ class LMTextButton extends StatefulWidget {
     this.width,
     this.margin,
     this.isActive = false,
+    this.placement = LMIconPlacement.start,
   });
 
   final bool isActive;
@@ -30,6 +33,7 @@ class LMTextButton extends StatefulWidget {
   final double borderRadius;
   final double? height;
   final double? width;
+  final LMIconPlacement placement;
 
   @override
   State<LMTextButton> createState() => _LMTextButtonState();
@@ -50,18 +54,33 @@ class _LMTextButtonState extends State<LMTextButton> {
           color: widget.backgroundColor ?? Colors.transparent,
           borderRadius: BorderRadius.circular(widget.borderRadius),
         ),
+        alignment: Alignment.center,
         child: Center(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              widget.isActive
-                  ? widget.activeIcon ?? const SizedBox()
-                  : widget.icon ?? const SizedBox(),
-              widget.icon != null || widget.activeIcon != null
-                  ? SizedBox(width: widget.margin ?? 8)
+              widget.placement == LMIconPlacement.start
+                  ? widget.isActive
+                      ? widget.activeIcon ?? const SizedBox()
+                      : widget.icon ?? const SizedBox()
+                  : const SizedBox(width: 8),
+              widget.placement == LMIconPlacement.start
+                  ? (widget.icon != null || widget.activeIcon != null)
+                      ? SizedBox(width: widget.margin ?? 8)
+                      : const SizedBox()
                   : const SizedBox(),
               widget.isActive ? widget.activeText ?? widget.text : widget.text,
+              widget.placement == LMIconPlacement.end
+                  ? widget.icon != null || widget.activeIcon != null
+                      ? SizedBox(width: widget.margin ?? 8)
+                      : const SizedBox()
+                  : const SizedBox(),
+              widget.placement == LMIconPlacement.end
+                  ? widget.isActive
+                      ? widget.activeIcon ?? const SizedBox()
+                      : widget.icon ?? const SizedBox()
+                  : const SizedBox(width: 8),
             ],
           ),
         ),
