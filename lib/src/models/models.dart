8ac2fc424c216448bfcadc8e0225aef6406550b8
Anurag Tyagi <anurag.tyagi@likeminds.community>
2023-12-05 13:34:22 +0530

    fix(ViewData): added missing export statement

--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -1,3 +1,25 @@
-export 'helper/media_model.dart';
 export 'post/topic_view_data.dart';
 export 'post/post_view_data.dart';
+
+export 'comment/comment_view_data.dart';
+
+export 'commons/like_view_data.dart';
+export 'commons/popup_menu_view_data.dart';
+
+export 'feed/feedroom_view_data.dart';
+
+export 'helper/media_model.dart';
+export 'helper/og_tags_view_data.dart';
+export 'helper/attachment/attachment_meta_view_data.dart';
+export 'helper/attachment/attachment_view_data.dart';
+export 'helper/tagging/group_tag_view_data.dart';
+export 'helper/tagging/user_tag_view_data.dart';
+
+export 'notification_feed/activity_entity_view_data.dart';
+export 'notification_feed/notification_feed_item_view_data.dart';
+
+export 'sdk/sdk_client_info_view_data.dart';
+
+export 'user/user_view_data.dart';
+
+export 'widget/widget_view_data.dart';
